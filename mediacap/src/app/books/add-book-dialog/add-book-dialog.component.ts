319d6cd25a2419b5518d1d25732bccac7b21aae4
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-10 10:03:20 +0200

    added book dialog components

---
+++ mediacap/src/app/books/add-book-dialog/add-book-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-book-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-book-dialog.component.html',
+  styleUrl: './add-book-dialog.component.scss'
+})
+export class AddBookDialogComponent {
+
+}
---
+++ mediacap/src/app/books/edit-book-dialog/edit-book-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-book-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-book-dialog.component.html',
+  styleUrl: './edit-book-dialog.component.scss'
+})
+export class EditBookDialogComponent {
+
+}
