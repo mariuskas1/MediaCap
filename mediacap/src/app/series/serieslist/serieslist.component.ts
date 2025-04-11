333437765c5b3daefa429bc1b3520013d48ea610
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-11 10:57:00 +0200

    basic series list functionality

--- mediacap/src/app/series/serieslist/serieslist.component.ts
+++ mediacap/src/app/series/serieslist/serieslist.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule} from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatMenuModule} from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -15,6 +15,7 @@ import { AddSeriesDialogComponent } from '../add-series-dialog/add-series-dialog
 import { EditSeriesDialogComponent } from '../edit-series-dialog/edit-series-dialog.component';
 import { Series } from '../../models/series.class';
 
+
 @Component({
   selector: 'app-serieslist',
   standalone: true,
@@ -23,5 +24,134 @@ import { Series } from '../../models/series.class';
   styleUrl: './serieslist.component.scss'
 })
 export class SerieslistComponent {
+
+  series: Series = new Series();
+  allUserSeries: Series[] = [];
+  sortedSeries: { [key: string]: Series[] } = {};
+  series$!: Observable<Series[]>;
+  
+  currentYear!: number;
+  currentMonth!: string;
+  months: string[] = [
+      'January', 'February', 'March', 'April', 'May', 'June', 
+      'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  years: number[] = [2025, 2024, 2023, 2022];
+  
+  userId: string | null = null;
+  
+  sortBy: string = 'default';
+  searchQuery?:string;
+  
+  constructor(private route: ActivatedRoute, public dialog: MatDialog, private firestore: Firestore) {}
+
+
+  openDialog(){
+    this.dialog.open(AddSeriesDialogComponent, {
+      data: {
+        month: this.currentMonth,
+        year: this.currentYear,
+        userId: this.userId,
+      }
+    });
+  }
+
+
+  openEditSeriesDialog(id:string | undefined){
+    this.dialog.open(EditSeriesDialogComponent, {
+      data: {
+        userId: this.userId,
+        seriesId: id,
+      }
+    });
+  }
+  
+
+  ngOnInit(){
+    this.setCurrentYearAndMonth();
+    this.getUserId();
+    this.initializeSeriesArrays();
+    this.getUserSeries();
+  }
+
+
+  initializeSeriesArrays(){
+    this.years.forEach(year => {
+      this.months.forEach(month => {
+        const key = `${month}_${year}`;
+        this.sortedSeries[key] = []; 
+      })
+    })
+  }
+
+
+  getUserSeries(){
+    const userSeriesCollection = collection(this.firestore, `series/${this.userId}/userSeries`);
+    this.series$ = collectionData(userSeriesCollection, { idField: 'id' }) as Observable<Series[]>;
+
+    this.series$.subscribe((changes) => {
+      this.allUserSeries = Array.from(new Map(changes.map(series => [series.id, series])).values());
+      this.populateSeriesArrays();
+      this.sortSeriesArrays();
+    })
+  }
+
+
+  populateSeriesArrays(){
+    Object.keys(this.sortedSeries).forEach((key) => {
+      this.sortedSeries[key] = []; 
+    });
+
+    this.allUserSeries.forEach(series => {
+      const month = series.monthWatched;
+      const year = series.yearWatched;
+
+      if (month && year) {
+        const key = `${month}_${year}`;
+        if (this.sortedSeries[key]) {
+          this.sortedSeries[key].push(series);
+        } else {
+          console.error(`No array found for key: ${key}`);
+        }
+      }
+    });
+  }
+
+
+  sortSeriesArrays(){
+    Object.keys(this.sortedSeries).forEach((key) => {
+      this.sortedSeries[key].sort((a, b) => {
+        // Compare series timestamps
+        const timestampA = new Date(a.timestamp).getTime();
+        const timestampB = new Date(b.timestamp).getTime();
+        return timestampA - timestampB; // Ascending order
+      });
+    });
+  }
+
+
+  setCurrentYearAndMonth(){
+    const currentDate = new Date(); 
+    this.currentYear = currentDate.getFullYear(); 
+    this.currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+  }
+
+
+  getUserId(){
+    this.route.parent?.paramMap.subscribe(params => {
+      this.userId = params.get('id'); 
+    });
+  }
+
+  onSearch(searchQuery:string | undefined){
+    console.log(searchQuery);
+  }
+
+  async deleteSeries(id:string | undefined){
+    const seriesDocRef = doc(this.firestore, `series/${this.userId}/userSeries/${id}`);
+    await deleteDoc(seriesDocRef).catch((err) => {
+      console.error(err);
+    });
+  }
 
 }
