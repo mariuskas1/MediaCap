fca51656f1370608111be1080a7f3885e54e571d
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-11 12:03:01 +0200

    basic dashboard functionality

--- mediacap/src/app/dashboard/dashboard.component.ts
+++ mediacap/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, docData } from '@angular/fire/firestore';
 import { MatCardModule } from '@angular/material/card';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book.class';
 
 
 @Component({
@@ -14,14 +16,55 @@ import { MatCardModule } from '@angular/material/card';
 export class DashboardComponent {
 
   userId: string | null = null;
+  currentYear?: number;
+
+  booksReadYear?: number; 
+  bookHighlights?: number;
+  bookGenres?: string[];
+  filmsWatchedYear?: number; 
+  filmHighlights?: number;
+  filmGenres?: string[];
+
+  books$!: Observable<Book[]>;
+  allUserBooks: Book[] = [];
+
+
 
   constructor(private route: ActivatedRoute, private firestore: Firestore){}
   
   
   ngOnInit(){
-    this.route.paramMap.subscribe(params => {
-      this.userId = params.get('id');
+    this.getUserId();
+    this.getBoardData();
+    this.setCurrentYear();
+  }
+
+  getUserId(){
+    this.route.parent?.paramMap.subscribe(params => {
+      this.userId = params.get('id'); 
+    });
+  }
+
+  getBoardData(){
+    this.subscibeToUserBooksCollection();
+  }
+
+  subscibeToUserBooksCollection(){
+    const userBooksCollection = collection(this.firestore, `books/${this.userId}/userBooks`);
+    this.books$ = collectionData(userBooksCollection, { idField: 'id' }) as Observable<Book[]>;
+  
+    this.books$.subscribe((changes) => {
+      this.allUserBooks = Array.from(new Map(changes.map(book => [book.id, book])).values());
+      console.log('All User Books:', this.allUserBooks);
+      const booksReadThisYearArray = this.allUserBooks.filter((book) => book.yearRead === this.currentYear);
+      console.log('Books Read This Year:', booksReadThisYearArray);
+      this.booksReadYear = booksReadThisYearArray.length;
+      console.log('Books Read Count:', this.booksReadYear);
     })
   }
 
+  setCurrentYear(){
+    const currentDate = new Date(); 
+    this.currentYear = currentDate.getFullYear(); 
+  }
 }
