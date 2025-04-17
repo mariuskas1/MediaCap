9dc411375d8e8424160644e9840ea6458ceb1bea
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-17 09:21:27 +0200

    basic films dashboard structure and functionality

--- mediacap/src/app/films/films.component.ts
+++ mediacap/src/app/films/films.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Film } from '../models/film.class';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-films',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIcon, MatButtonModule, MatTooltipModule, MatListModule, FormsModule, MatInputModule,],
   templateUrl: './films.component.html',
   styleUrl: './films.component.scss'
 })
 export class FilmsComponent {
+
+  userId: string | null = null;
+  currentYear?: number;
+  currentMonth?: string;
+
+  filmsWatchedYear?: number; 
+  filmHighlights?: number;
+  filmGenresMap: { [genre: string]: Film[] } = {};
+  favoriteFilmGenres: string[] = [];
+
+  films$!: Observable<Film[]>;
+  allUserFilms: Film[] = [];
 
 }
