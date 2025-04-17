259d57954467b514d2929a0c168d98f4008485d5
Marius Kasparek <marius.kasparek@gmail.com>
2025-04-17 08:55:44 +0200

    basic book dashboard template and functionality

--- mediacap/src/app/books/books.component.ts
+++ mediacap/src/app/books/books.component.ts
@@ -1,12 +1,144 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { Book } from '../models/book.class';
+import { Observable } from 'rxjs';
+import { Film } from '../models/film.class';
+import { Series } from '../models/series.class';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule } from '@angular/forms';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule, MatIcon, MatButtonModule,  MatTooltipModule, FormsModule],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss'
 })
 export class BooksComponent {
+  @ViewChild('addBookInput') addBookInput!: ElementRef<HTMLInputElement>;
+  @ViewChildren('bookInput') bookInputs!: QueryList<ElementRef<HTMLInputElement>>;
 
+  selectedBookOptionsIndex: number | null = null;
+  editCurrentBookIndex: number | null = null;
+
+  booksReadYear?: number; 
+  bookHighlights?: number;
+  bookGenresMap: { [genre: string]: Book[] } = {};
+  favoriteBookGenres: string[] = [];
+
+  books$!: Observable<Book[]>;
+  allUserBooks: Book[] = [];
+
+  currentBooks$!: Observable<Book[]>;
+  currentBooks: Book[] = [];
+  newCurrentBook = new Book();
+  showAddBookInput = false;
+
+  userId: string | null = null;
+  currentYear?: number;
+  currentMonth?: string;
+
+
+  constructor(private route: ActivatedRoute, private firestore: Firestore, public dialog: MatDialog){}
+
+
+  ngOnInit(){
+    this.getUserId();
+    this.getBoardData();
+    this.setCurrentDate();
+  }
+
+  getUserId(){
+    this.route.parent?.paramMap.subscribe(params => {
+      this.userId = params.get('id'); 
+    });
+  }
+
+  getBoardData(){
+    this.subscribeToUserBooksCollection();
+    this.subscribeToCurrentBooksCollection();
+  }
+
+   subscribeToUserBooksCollection(){
+    const userBooksCollection = collection(this.firestore, `books/${this.userId}/userBooks`);
+    this.books$ = collectionData(userBooksCollection, { idField: 'id' }) as Observable<Book[]>;
+  
+    this.books$.subscribe((changes) => {
+      this.getBookStats(changes);
+    })
+  }
+
+  subscribeToCurrentBooksCollection(){
+    const currentBooksCollection = collection(this.firestore, `books/${this.userId}/currentBooks`);
+    this.currentBooks$ = collectionData(currentBooksCollection, { idField: 'id' }) as Observable<Book[]>;
+  
+    this.currentBooks$.subscribe((changes) => {
+      this.currentBooks = Array.from(new Map(changes.map(book => [book.id, book])).values());
+      this.currentBooks.sort((a, b) => a.timestamp - b.timestamp);
+    })
+  }
+
+  getBookStats(changes: Book[]){
+    this.allUserBooks = Array.from(new Map(changes.map(book => [book.id, book])).values());
+    const booksReadThisYearArray = this.allUserBooks.filter((book) => book.yearRead === this.currentYear);
+    this.booksReadYear = booksReadThisYearArray.length;
+    const bookHighlightsArray = booksReadThisYearArray.filter((book) => book.favorite === true);
+    this.bookHighlights = bookHighlightsArray.length;
+
+    this.sortBooksAfterGenres(booksReadThisYearArray);
+  }
+
+  sortBooksAfterGenres(booksReadThisYearArray: Book[]){
+    this.bookGenresMap = {};
+
+    booksReadThisYearArray.forEach(book => {
+      if (Array.isArray(book.genres)) {
+        book.genres.forEach(genre => {
+          if (!this.bookGenresMap[genre]) {
+            this.bookGenresMap[genre] = [];
+          }
+          this.bookGenresMap[genre].push(book);
+        });
+      }
+    });
+
+    this.determineFavoriteBookGenres();
+  }
+
+  determineFavoriteBookGenres(){
+    const genreCounts = Object.entries(this.bookGenresMap).map(
+      ([genre, books]) => ({ genre, count: books.length })
+    );
+
+    genreCounts.sort((a, b) => b.count - a.count);
+    const topGenres = genreCounts.slice(0, 2).map(entry => entry.genre);
+    this.favoriteBookGenres = topGenres;
+  }
+
+  showAddBookInputDiv(){
+    this.showAddBookInput = true;
+    setTimeout(() => {
+      this.addBookInput?.nativeElement?.focus();
+    }, 50);
+  }
+
+  
+
+  hideAddBookInputDiv(){
+    this.showAddBookInput = false;
+    this.newCurrentBook.title = '';
+  }
+
+  setCurrentDate(){
+    const currentDate = new Date(); 
+    this.currentYear = currentDate.getFullYear(); 
+    this.currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+
+  }
 }
