2260e3ad95cce11657407421a7383713160b8354
Marius <marius.kasparek@gmail.com>
2025-04-21 13:03:10 +0200

    added basic header for login page

--- mediacap/src/app/login-layout/login-layout.component.ts
+++ mediacap/src/app/login-layout/login-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-layout',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, MatToolbarModule],
   templateUrl: './login-layout.component.html',
   styleUrl: './login-layout.component.scss'
 })
